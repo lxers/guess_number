128d8e48fdd1c10ddf8b0b14c43f7bc27484bf12
Максим Баженов <bazenov690@gmail.com>
2025-10-27 21:53:26 +0300

    Add new empty function.

--- guess_number.py
+++ guess_number.py
@@ -7,10 +7,13 @@ def guess():
 
         if num_rand > num: 
             print('Выше')
-        elif num_rand < num:
+        elif num_rand < num: 
             print('Ниже')
         else:
             print('ВЫ УГАДАЛИ!')
             break
+
+def empty(): # Тут я добавил пустую функцию потому что в функции guess() у меня уже стоял elif. 
+    pass 
 
 guess()
