05cb120456aabf33ad05da2a762f5798ba69ea1a
Максим Баженов <bazenov690@gmail.com>
2025-10-27 21:46:39 +0300

    add initial commit

---
+++ guess_number.py
@@ -0,0 +1,16 @@
+from random import randint
+
+def guess():
+    num_rand = randint(1, 100)
+    while True:
+        num = int(input('Введите число: '))
+
+        if num_rand > num: 
+            print('Выше')
+        elif num_rand < num:
+            print('Ниже')
+        else:
+            print('ВЫ УГАДАЛИ!')
+            break
+
+guess()
